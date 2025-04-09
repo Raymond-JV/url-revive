d65d5d102176ceb122c498b30814b3c897f11f92
Raymond Van Wart <23642921+Raymond-JV@users.noreply.github.com>
2025-04-09 01:37:06 -0500

    Switch to Poetry

--- config/config.py
+++
@@ -1,8 +0,0 @@
-from dynaconf import Dynaconf
-
-settings = Dynaconf(
-    settings_files=['settings.toml', '.secrets.toml']
-)
-
-# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
-# `settings_files` = Load these files in the order.
